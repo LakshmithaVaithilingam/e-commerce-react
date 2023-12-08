8a244c9261f0ce78ada2578a00b63a80a527a1fd
Lakshmitha Vaithilingam <lakshmitha.20210713@iit.ac.lk>
2023-12-08 13:52:48 +0530

    Update app.js
    Deleted the other options

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 // Import React, React Router, and the page components
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
 import ProductManagement from './pages/ProductManagement';
-// import OrderManagement from './pages/OrderManagement';
-// import Complaints from './pages/Complaints';
-// import UserManagement from './pages/UserManagement';
-// import AdminManagement from './pages/AdminManagement';
-// import EventsActivities from './pages/EventsActivities';
-// import Settings from './pages/Settings';
 import Navbar from './components/Navbar';
 import LeftPanel from './components/LeftPanel';
+import Category from './pages/Category';
 
 // Create the App component using the page components and the routes
 const App = () => {
@@ -19,9 +13,8 @@ const App = () => {
       <Navbar />
       <LeftPanel />
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/product-management" element={<ProductManagement />} />
-        {/* <Route path="/order-management" element={<OrderManagement />} /> */}
+        <Route path="/category" element={<Category />} />
         {/* <Route path="/complaints" element={<Complaints />} /> */}
         {/* <Route path="/user-management" element={<UserManagement />} /> */}
         {/* <Route path="/admin-management" element={<AdminManagement />} /> */}
