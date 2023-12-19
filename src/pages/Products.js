4ec495d62a306c63dee4d7eb6a6c28efb6bafaad
Lakshmitha Vaithilingam <lakshmitha.20210713@iit.ac.lk>
2023-12-19 21:59:51 +0530

    Update product.js
    added filters

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -60,17 +60,115 @@ const StyledButton = styled.button`
   }
 `;
 
+const Form = styled.form`
+  margin-bottom: 20px;
+  padding: 15px;
+  border: 1px solid #ddd;
+  background-color: #f9f9f9;
+  border-radius: 5px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 15px;
+  align-items: center;
+
+  label {
+    margin-right: 10px;
+    margin-bottom: 15px;
+    
+  }
+
+  select, input {
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    margin-bottom: 10px;
+  }
+
+  button {
+    background-color: #333;
+    color: #fff;
+    border: none;
+    margin-bottom: 10px;
+    padding: 10px 15px;
+    cursor: pointer;
+    border-radius: 4px;
+    transition: background-color 0.2s ease-in-out;
+
+    &:hover {
+      background-color: #555;
+    }
+  }
+`;
+
 const ProductsPage = () => {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [subcategories, setSubcategories] = useState([]);
+  const [filters, setFilters] = useState({
+    category_id: '',
+    subcategory_id: '',
+    age_group: '',
+  });
 
   useEffect(() => {
     // Fetch the list of products when the component mounts
+    fetchCategories();
     fetchProducts();
   }, []);
 
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/api/category');
+      const data = await response.json();
+      if (data.status === 200) {
+        setCategories(data.categories);
+      } else {
+        console.error('Error fetching categories:', data.message);
+      }
+    } catch (error) {
+      console.error('Error fetching categories:', error);
+    }
+  };
+
+
+  // Fetch subcategories based on selected category
+  useEffect(() => {
+    if (filters.category_id) {
+      fetchSubcategories(filters.category_id);
+    } else {
+      setSubcategories([]); // Reset subcategories when no category is selected
+    }
+  }, [filters.category_id]);
+
+  // Fetch subcategories from the backend
+  const fetchSubcategories = async (categoryId) => {
+    try {
+      const response = await axios.get(`http://localhost:8000/api/category/${categoryId}/subcategories`);
+      const data = response.data;
+      if (data.status === 200) {
+        setSubcategories(data.subcategories);
+      } else {
+        console.error('Error fetching subcategories:', data.message);
+      }
+    } catch (error) {
+      console.error('Error fetching subcategories:', error);
+    }
+  };
+
   const fetchProducts = async () => {
     try {
-      const response = await axios.get('http://localhost:8000/api/products');
+      let response;
+  
+      // Check if filters are present
+      if (Object.values(filters).some(Boolean)) {
+        response = await axios.get('http://localhost:8000/api/products', {
+          params: filters,
+        });
+      } else {
+        // Fetch all products when no filters are present
+        response = await axios.get('http://localhost:8000/api/products');
+      }
+  
       const data = response.data;
       if (data.status === 200) {
         setProducts(data.products);
@@ -80,6 +178,11 @@ const ProductsPage = () => {
     } catch (error) {
       console.error('Error fetching products:', error);
     }
+  };
+
+  // Function to apply filters and fetch filtered products
+  const applyFilters = () => {
+    fetchProducts();
   };
 
   const [cart, setCart] = useState({}); // Use an object to track the cart status for each product
@@ -94,6 +197,68 @@ const ProductsPage = () => {
   return (
     <div>
       <h1>Products</h1>
+      {/* Filter form */}
+      <Form>
+        {/* Category filter */}
+        <label htmlFor="category">Category:</label>
+        <select
+          id="category"
+          value={filters.category_id}
+          onChange={(e) => setFilters({ ...filters, category_id: e.target.value })}
+        >
+          <option value="">All Categories</option>
+          {categories.map((category) => (
+            <option key={category.category_id} value={category.category_id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+
+        {/* Subcategory filter */}
+        <label htmlFor="subcategory">Subcategory:</label>
+        <select
+          id="subcategory"
+          value={filters.subcategory_id}
+          onChange={(e) => setFilters({ ...filters, subcategory_id: e.target.value })}
+        >
+          <option value="">All Subcategories</option>
+          {subcategories.map((subcategory) => (
+            <option key={subcategory.subcategory_id} value={subcategory.subcategory_id}>
+              {subcategory.name}
+            </option>
+          ))}
+        </select>
+
+        {/* Age group filter */}
+        <label htmlFor="ageGroup">Age Group:</label>
+        <input
+          type="text"
+          id="ageGroup"
+          value={filters.age_group}
+          onChange={(e) => setFilters({ ...filters, age_group: e.target.value })}
+        />
+
+        {/* Price range filters */}
+        <label htmlFor="priceMin">Price Min:</label>
+        <input
+          type="number"
+          id="priceMin"
+          value={filters.price_min}
+          onChange={(e) => setFilters({ ...filters, price_min: e.target.value })}
+        />
+        <label htmlFor="priceMax">Price Max:</label>
+        <input
+          type="number"
+          id="priceMax"
+          value={filters.price_max}
+          onChange={(e) => setFilters({ ...filters, price_max: e.target.value })}
+        />
+
+        {/* Apply button */}
+        <button type="button" onClick={applyFilters}>
+          Apply Filters
+        </button>
+      </Form>
       <ProductsContainer>
         {products.map((product) => (
           <ProductCard key={product.products_id}>
